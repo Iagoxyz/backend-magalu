6ce826b5c9e405218231e159a7fbccc07e8bcd96
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-08 13:30:04 -0300

    Project created

---
+++ src/test/java/tech/backend/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.backend.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
