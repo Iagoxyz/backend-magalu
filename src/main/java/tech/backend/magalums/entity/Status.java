fe79069e687b4b5238966411dadcc8d12d020c97
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-08 13:55:47 -0300

    Mapping Notification Entity, Channel, and Status

---
+++ src/main/java/tech/backend/magalums/entity/Status.java
@@ -0,0 +1,14 @@
+package tech.backend.magalums.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_status")
+public class Status {
+
+    @Id
+    private Long statusId;
+    private String description;
+}
