ed100535efc530e2bd7479f20cf1165610252e23
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-02-10 12:00:14 -0300

    Notification insert

--- src/main/java/tech/backend/magalums/service/NotificationService.java
+++ src/main/java/tech/backend/magalums/service/NotificationService.java
@@ -3,6 +3,7 @@ package tech.backend.magalums.service;
 import org.springframework.stereotype.Service;
 import tech.backend.magalums.controller.dto.ScheduleNotificationDTO;
 import tech.backend.magalums.entity.Notification;
+import tech.backend.magalums.entity.Status;
 import tech.backend.magalums.repository.NotificationRepository;
 
 import java.util.Optional;
@@ -22,5 +23,14 @@ public class NotificationService {
 
     public Optional<Notification> findById(Long notificationId) {
         return notificationRepository.findById(notificationId);
+    }
+
+    public void cancelNotification(Long notificationId) {
+        var notification = findById(notificationId);
+
+        if (notification.isPresent()) {
+            notification.get().setStatus(Status.Values.CANCELED.toStatus());
+            notificationRepository.save(notification.get());
+        }
     }
 }
